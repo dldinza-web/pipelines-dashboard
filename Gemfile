ff05fe9e913fc2e70c17e1a2e9c81ad907646599
Dennys Lopez Dinza <dldinza.web@gmail.com>
2023-01-16 16:43:55 -0500

    Database Cleaner

--- Gemfile
+++ Gemfile
@@ -73,3 +73,7 @@ group :development do
   # gem "spring"
 end
 
+group :test do
+  gem 'database_cleaner-active_record'
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_204822) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
