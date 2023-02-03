20287d3b8af9fb46ec58c6c480287ce8af3511e8
Dennys Lopez Dinza <dldinza.web@gmail.com>
2023-02-03 08:57:45 -0500

    Redirect to Login when the user tries to nagivate without authentication

--- app/javascript/src/components/pages/projects_pipeline/projects_pipeline.page.tsx
+++ app/javascript/src/components/pages/projects_pipeline/projects_pipeline.page.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppLayout from 'src/components/layout/app.layout';
+import { CURRENT_USER_LSTG_KEY } from '../sign-in/constants/sign-in.constants';
 import ProjectList from './components/project-list/project-list';
 
 const ProjectsPipelinePage = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!localStorage.getItem(CURRENT_USER_LSTG_KEY)) {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <AppLayout title="Projects">
       <ProjectList />
